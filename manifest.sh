7630306ecb3f43f713d893c1ebc66f8d0dbd7ea2
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-12-04 18:27:41 +0000

    Corrected manifest tag

--- manifest.sh
+++ manifest.sh
@@ -6,7 +6,7 @@ docker login >> /dev/null 2>&1
 
 VERSIONS=($(find . -type d -regex '\./[a-z].+' | sort | sed 's/\.\///'))
 for VERSION in "${VERSIONS[@]}"; do
-  TAG="sykescottages/cdk:${VERSION}"
+  TAG="sykescottages/scratch:${VERSION}"
   docker manifest create $TAG \
     --amend "$TAG-amd64" \
     --amend "$TAG-arm64"
