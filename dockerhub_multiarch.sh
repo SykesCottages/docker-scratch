b5c17142bd13f36a41b317563c92c95f6575994e
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-12-15 15:36:15 +0000

    Fixes the missing architecture argument

--- dockerhub_multiarch.sh
+++ dockerhub_multiarch.sh
@@ -3,6 +3,15 @@
 # You need to provide your own creds because #security
 docker login >> /dev/null 2>&1
 
+if [ "x86_64" == "$ARCH" ]; then
+  ARCH=amd64
+elif [ "aarch64" == "$ARCH" ]; then
+  ARCH=arm64
+else
+  echo "Architecture ${ARCH} not supported."
+  exit 1
+fi
+
 VERSIONS=( "php-5.6-fpm-e-dev" "node-12-e-dev" )
 for VERSION in "${VERSIONS[@]}"
 do
