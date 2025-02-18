42ad5f1d69b13100b59b85e6297c9f7212dc77ad
Andrew Beattie <andrew.beattie@sykescottages.co.uk>
2025-02-18 16:25:10 +0000

    Adding 8.3 Adding 8.4

---
+++ php-8.3-cli-h-dev/run_tests.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+PHP_VERSION=8.3
+
+itShouldCheckThatAllInstalledSoftwareExists() {
+    doesCommandExist php
+    doesCommandExist composer
+}
+
+itShouldCheckThatAllPhpModulesExists() {
+  doesPhpModuleExist ldap
+  doesPhpModuleExist mysqli
+  doesPhpModuleExist gd
+  doesPhpModuleExist mcrypt
+  doesPhpModuleExist amqp
+}
+
+itShouldMatchTheDesiredVersionOfPhp() {
+  doesPhpMatchVersion $PHP_VERSION
+}
+
+doesCommandExist() {
+    if ! [ -x "$(command -v $1)" ]; then
+      echo "Error: $1 is not installed." >&2
+      exit 1
+    else
+      echo "$1 Command Exists"
+    fi
+}
+
+doesPhpModuleExist() {
+    MODULE=$(php -m|grep -e "^$1$")
+    if [ $1 != "$MODULE" ]; then
+      echo "Error: $1 PHP module is not installed." >&2
+      exit 1
+    else
+      echo "$1 Module Exists"
+    fi
+}
+
+doesPhpMatchVersion() {
+    VERSION=$(php -v|grep -i "PHP $1")
+    if [ -z "$VERSION" ]; then
+      echo "Error: PHP version $1 is not found." >&2
+      exit 1
+    else
+      echo "PHP $1 Exists"
+    fi
+}
+
+itShouldCheckThatAllInstalledSoftwareExists
+itShouldCheckThatAllPhpModulesExists
+itShouldMatchTheDesiredVersionOfPhp
